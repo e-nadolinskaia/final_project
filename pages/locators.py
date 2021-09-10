ee6d68dda263ca157cbda74c3f87df87ae402b76
Ekaterina <spiritunity@rambler.ru>
2021-09-10 18:47:17 +0300

    Creating locators for ProductPage

--- pages/locators.py
+++ pages/locators.py
@@ -6,3 +6,10 @@ class MainPageLocators():
 class LoginPageLocators():
 	LOGIN_FORM_ELEMENT = (By.CSS_SELECTOR, '#login_form')
 	REGISTER_FORM = (By.CSS_SELECTOR, '#register_form')
+
+class ProductPageLocators():
+	ADDING_LINK = (By.CSS_SELECTOR,'.btn-add-to-basket')
+	MESSAGE_ADDED_BOOK_NAME = (By.CSS_SELECTOR, 'div[id="messages"] :nth-child(2) > strong')
+	BOOK_NAME  = (By.CSS_SELECTOR, '.product_main h1')
+	BASKET_COST = (By.CSS_SELECTOR,'div[id="messages"] :nth-child(3) p > strong')
+	PRODUCT_PRICE = (By.CSS_SELECTOR,'.product_main :nth-child(2)')
