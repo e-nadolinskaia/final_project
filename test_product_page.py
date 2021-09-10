19be386723b3e642df09047385d2d5f5b08e408c
Ekaterina <spiritunity@rambler.ru>
2021-09-10 21:10:51 +0300

    Launching test_guest_can_add_product_to_basket() with parametrize url

--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,9 @@
 from .pages.product_page import ProductPage
+import pytest
+import time
 
 
-def test_guest_can_add_product_to_basket(browser):
+'''def test_guest_can_add_product_to_basket(browser):
 	link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
 	page = ProductPage(browser,link)
 	browser.implicitly_wait(10)
@@ -25,5 +27,16 @@ def test_compairing_basket_cost_and_product_price(browser):
 	page.open()
 	page.add_product()
 	page.solve_quiz_and_get_code()
-	page.cost_of_basket_equal_product_price()
+	page.cost_of_basket_equal_product_price()'''
 	
+
+@pytest.mark.parametrize('links', ['0','1','2',pytest.param("3", marks=pytest.mark.xfail),'4','5','6',pytest.param("7", marks=pytest.mark.xfail),'8','9'])
+def test_guest_can_add_product_to_basket(browser,links):
+	link= f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{links}'
+	page = ProductPage(browser,link)
+	browser.implicitly_wait(10)
+	page.open()
+	page.add_product()
+	page.solve_quiz_and_get_code()
+	page.cost_of_basket_equal_product_price()
+	page.name_added_product_is_equal_name_in_message()
