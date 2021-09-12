28421a0d135995b2de9a8eab1bedb7f8761421f1
Ekaterina <spiritunity@rambler.ru>
2021-09-12 21:00:16 +0300

    Adding tests of presence and disappereaning elements

--- test_product_page.py
+++ test_product_page.py
@@ -27,7 +27,7 @@ def test_compairing_basket_cost_and_product_price(browser):
 	page.open()
 	page.add_product()
 	page.solve_quiz_and_get_code()
-	page.cost_of_basket_equal_product_price()'''
+	page.cost_of_basket_equal_product_price()
 	
 
 @pytest.mark.parametrize('links', ['0','1','2',pytest.param("3", marks=pytest.mark.xfail),'4','5','6',pytest.param("7", marks=pytest.mark.xfail),'8','9'])
@@ -39,4 +39,29 @@ def test_guest_can_add_product_to_basket(browser,links):
 	page.add_product()
 	page.solve_quiz_and_get_code()
 	page.cost_of_basket_equal_product_price()
-	page.name_added_product_is_equal_name_in_message()
+	page.name_added_product_is_equal_name_in_message()'''
+
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+	link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/'
+	page = ProductPage(browser,link)
+	page.open()
+	page.add_product()
+	page.solve_quiz_and_get_code()
+	page.should_not_be_success_message()
+
+def test_guest_cant_see_success_message(browser):
+	link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/'
+	page = ProductPage(browser,link)
+	page.open()
+	page.should_not_be_success_message()
+
+def test_message_disappeared_after_adding_product_to_basket(browser):
+	link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/'
+	page = ProductPage(browser,link)
+	page.open()
+	page.add_product()
+	page.solve_quiz_and_get_code()
+	page.message_disappeared_after_adding_product()
+
+
